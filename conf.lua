99bc66e709f0da936215612f1ce4e3fae627ea92
Sohil876 <sohil876@gmail.com>
2021-07-31 02:09:27 +0530

    Halo Warld!

--- conf.lua
+++ conf.lua
@@ -1,5 +1,10 @@
 
 function love.conf(t)
-    
+    t.title = "TestWorld"
+    t.version = "11.3" -- Love2D version
+    t.console = false -- Not available in android builds :/
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.vsync = 0 -- 1=Enable, -1=Adaptive, 0=Disable
 end
 
--- main.lua
+++ main.lua
@@ -1,14 +1,26 @@
 -- TestWorld
 
 function love.load()
-    
+    font = love.graphics.newFont(17)
+    str = "Halo Warld!"
+    x = love.graphics.getWidth() / 2
+    y = love.graphics.getHeight() / 2
 end
 
 function love.update()
-    
+    --
+end
+
+function love.keypressed(key, scancode, isrepeat)
+   if key == "escape" then
+      love.event.quit()
+      love.quit()
+      os.exit()
+   end
 end
 
 function love.draw()
-    
+    love.graphics.setFont(font)
+    love.graphics.printf(str, x, y, font:getWidth(str), "center")
 end
 
